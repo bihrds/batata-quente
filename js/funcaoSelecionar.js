7e3b918b533daa1ac167b1d0d567d591d2ad1749
Antônio José Martins Mascarenhas <amartinsmascarenhas@gmail.com>
2023-09-28 11:17:11 -0300

    Correção de bugs e otimização do código
    Foi adicionado um aviso ao jogador se ele não selecionar um modo ou a quantidade de jogadores, e adicionei a função para isso funcionar, e corrigi um bug que ocorria ao selecionar um modo multplayer e mudava para o solo

--- js/funcaoSelecionar.js
+++ js/funcaoSelecionar.js
@@ -1,61 +1,80 @@
 const tituloNmrJogadoresHTML = document.querySelector('.nmrJogadores')
 const botoesQuantidadeJogadoreHTML = document.querySelector('.container-quantidade')
 const botaoIniciarHTML = document.querySelector('.iniciarJogo')
+const avisoHTML = document.querySelector('.aviso')
 
 const botaoSoloHTML = document.querySelector('.solo')
+const botaoVersusHTML = document.querySelector('.versus')
+const botaoContraoTempoHTML = document.querySelector('.contra-o-tempo')
+
 let soloSelecionado = false
+let versusSelecionado = false
+let contraTempoSelecionado = false
+
 const SelecionarSolo = () => {
     soloSelecionado = true
+    versusSelecionado = false
+    contraTempoSelecionado = false
+
+    quantidadeJogadores = 1
+
+    botaoContraoTempoHTML.className = 'botao-modo contra-o-tempo'
+    botaoVersusHTML.className = 'botao-modo versus'
+    botaoSoloHTML.className = 'botao-modo solo selecionado'
+    
+    botaoDoisJogadoresHTML.className = 'botao-quantidade dois-jogadores'
+    botaoTresJogadoresHTML.className = 'botao-quantidade tres-jogadores'
+    botaoQuatroJogadoresHTML.className = 'botao-quantidade qutro-jogadores'
+    
+    tituloNmrJogadoresHTML.style.display = 'none'
+    botoesQuantidadeJogadoreHTML.style.display = 'none'
 }
 
-const botaoVersusHTML = document.querySelector('.versus')
-let versusSelecionado = false
+
+
 const SelecionarVersus = () => {
+    soloSelecionado = false
     versusSelecionado = true
+    contraTempoSelecionado = false
+
+    botaoContraoTempoHTML.className = 'botao-modo contra-o-tempo '
+    botaoVersusHTML.className = 'botao-modo versus selecionado'
+    botaoSoloHTML.className = 'botao-modo solo'
+    
+    tituloNmrJogadoresHTML.style.display = 'block'
+    botoesQuantidadeJogadoreHTML.style.display = 'block'
 }
 
-const botaoContraoTempoHTML = document.querySelector('.contra-o-tempo')
-let contraTempoSelecionado = false
+
 const SelecionarContraTempo = () => {
+    soloSelecionado = false
+    versusSelecionado = false
     contraTempoSelecionado = true
+
+    botaoContraoTempoHTML.className = 'botao-modo contra-o-tempo selecionado'
+    botaoVersusHTML.className = 'botao-modo versus'
+    botaoSoloHTML.className = 'botao-modo solo'
+
+    tituloNmrJogadoresHTML.style.display = 'block'
+    botoesQuantidadeJogadoreHTML.style.display = 'block'
 }
  
-botaoSoloHTML.addEventListener('click', ()=> {
+botaoSoloHTML.addEventListener('click', () => {
     SelecionarSolo()
-    if(soloSelecionado){
-        botaoContraoTempoHTML.className = 'botao-modo contra-o-tempo'
-        botaoVersusHTML.className = 'botao-modo versus'
-        botaoSoloHTML.className = 'botao-modo solo selecionado'
-        tituloNmrJogadoresHTML.style.display = 'none'
-        botoesQuantidadeJogadoreHTML.style.display = 'none'
-    }
 })
 
 botaoVersusHTML.addEventListener('click', ()=> {
     SelecionarVersus()
-    if(versusSelecionado){
-        botaoContraoTempoHTML.className = 'botao-modo contra-o-tempo '
-        botaoVersusHTML.className = 'botao-modo versus selecionado'
-        botaoSoloHTML.className = 'botao-modo solo'
-        tituloNmrJogadoresHTML.style.display = 'block'
-        botoesQuantidadeJogadoreHTML.style.display = 'block'
-    }
 })
 
 botaoContraoTempoHTML.addEventListener('click', ()=> {
     SelecionarContraTempo()
-    if(contraTempoSelecionado){
-        botaoContraoTempoHTML.className = 'botao-modo contra-o-tempo selecionado'
-        botaoVersusHTML.className = 'botao-modo versus'
-        botaoSoloHTML.className = 'botao-modo solo'
-        tituloNmrJogadoresHTML.style.display = 'block'
-        botoesQuantidadeJogadoreHTML.style.display = 'block'
-    }
 })
 
 const botaoDoisJogadoresHTML = document.querySelector('.dois-jogadores')
 const botaoTresJogadoresHTML = document.querySelector('.tres-jogadores')
 const botaoQuatroJogadoresHTML = document.querySelector('.quatro-jogadores')
+
 let quantidadeJogadores = 1
 
 botaoDoisJogadoresHTML.addEventListener('click', ()=>{
@@ -75,12 +94,16 @@ botaoTresJogadoresHTML.addEventListener('click', ()=>{
 botaoQuatroJogadoresHTML.addEventListener('click', ()=>{
     botaoDoisJogadoresHTML.className = 'botao-quantidade dois-jogadores'
     botaoTresJogadoresHTML.className = 'botao-quantidade tres-jogadores'
-    botaoQuatroJogadoresHTML.className = 'botao-quantidade qutro-jogadores selecionado'
-    quantidadeJogadores = 2
+    botaoQuatroJogadoresHTML.className = 'botao-quantidade quatro-jogadores selecionado'
+    quantidadeJogadores = 4
 })
 
 botaoIniciarHTML.addEventListener('click', ()=>{
     if(!soloSelecionado && !versusSelecionado && !contraTempoSelecionado){
+        avisoHTML.innerHTML = '*Selecione um modo'
+        return
+    } else if((versusSelecionado || contraTempoSelecionado) && quantidadeJogadores == 1){
+        avisoHTML.innerHTML = '*Selecione a quantidade de jogadores'
         return
     }
     const modoSelecionado = soloSelecionado ? 'solo' : versusSelecionado ? 'versus' : 'contra-o-tempo'
